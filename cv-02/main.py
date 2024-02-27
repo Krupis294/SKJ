0eca8b573ed08defedca4dbc72c053e97e9e8c69
Krupis <lengsfeldkuba@gmail.com>
2024-02-27 13:09:26 +0100

    cv-02 Updated

--- cv-02/main.py
+++ cv-02/main.py
@@ -40,3 +40,86 @@ def super_print(*arg):
     print(separator)
     pass
 super_print("Hello", "World!")
+
+#Files
+
+def read_file(filename):
+    """
+    Returns content of a file as a string.
+
+    Uses classic file access.
+    """
+
+    f = None
+    lines = 'Unable to open a file: {0}'.format(filename)
+    try:
+        with open(filename, mode='r') as f:
+            lines = f.read()
+        pass
+    except FileNotFoundError:
+        print('File not found: {0}'.format(filename))
+        pass
+    finally:
+        return lines
+
+
+def read_file_using_with(filename):
+    """
+    Returns content of a file as a string.
+
+    Uses with statement to open a file.
+    """
+
+    lines = 'Unable to open a file: {0}'.format(filename)
+    try:
+        pass
+    except FileNotFoundError:
+        pass
+    finally:
+        return lines
+
+
+def filter_cities(filename, **kw):
+    """
+    Returns a list of cities that match their population specified by restrictions.
+    In **kw it will be possible to set an argument:
+     'gt': only cities with a population greater than the value of the argument
+     'lt': only cities with a population less than the value of the argument
+    It is possible to enter none, one or both parameters at once.
+    If no parameter is specified, an empty list is returned.
+    Use list comprehension for filtering.
+    Use so-called "unpacking" to load data.
+
+    Reimplement the function to return a generator.
+    """
+
+    cities = read_file(filename)
+    filtered = cities.split('\n')
+    #print(filtered)
+    if 'gt' in kw:
+        return [x.split(':')[0] for x in filtered if int(x.split(':')[1]) > kw['gt']]
+    if 'lt' in kw:
+        return [x.split(':')[0] for x in filtered if int(x.split(':')[1]) < kw['lt']]
+
+def main():
+    """
+    Main function.
+    This function is run at the startup of a script thanks to the cryptic if statement below.
+    """
+
+    # file reading
+    filename = 'cities.txt'
+    content = read_file(filename)
+    print('{} file content is: {}'.format(filename, content))
+
+    #content = read_file_using_with(filename)
+    #print('{} file content using with statement is: {}'.format(filename, content))
+
+    # filtering
+    print(filter_cities(filename, gt=500000))
+    print(filter_cities(filename, lt=300000))
+    print(filter_cities(filename, lt=300000, gt=500000))
+
+
+if __name__ == '__main__':
+    main()
