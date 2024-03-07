8f821002340385a6243c0aeaa139166a4a6f8d61
Krupis <lengsfeldkuba@gmail.com>
2024-03-07 21:06:55 +0100

    Done homework_02

--- cv-02/homework_02.py
+++ cv-02/homework_02.py
@@ -7,7 +7,10 @@ def is_palindrome(data):
         is_palindrome('aba') == True
         is_palindrome('abc') == False
     """
-
+    for i in range(len(data)):
+        if data[i] != data[-i-1]:
+            return False
+    return True
 
 def lex_compare(a, b):
     """
@@ -20,7 +23,16 @@ def lex_compare(a, b):
         lex_compare('ahoj', 'ahojky') == 'ahoj'
         lex_compare('dum', 'automobil') == 'automobil'
     """
-
+    min_len = min(len(a), len(b))
+    for i in range(min_len):
+        if a[i] != b[i]:
+            if a[i] < b[i]:
+                return a
+            else:
+                return b
+    if len(a) <= len(b):
+        return a
+    return b
 
 def count_successive(string):
     """
@@ -31,7 +43,20 @@ def count_successive(string):
           count_successive("aaabbcccc") == [("a", 3), ("b", 2), ("c", 4)]
           count_successive("aba") == [("a", 1), ("b", 1), ("a", 1)]
     """
-
+    if not string:
+        return []
+    count = 1
+    prev = string[0]
+    list = []
+    for i in range(1,len(string)):
+        if string[i] == prev:
+            count += 1
+        else:
+            list.append((prev, count))
+            prev = string[i]
+            count = 1
+    list.append((prev, count))
+    return list
 
 def find_positions(items):
     """
@@ -46,7 +71,19 @@ def find_positions(items):
             1: [1, 2]
         }
     """
+    if not items:
+        return {}
+    dict = {}
+    pos = 0
+    for i in range(1,len(items)+1):
+        if items[0] in dict:
+            dict[items[0]].append(pos)
+        else:
+            dict[items[0]] = [pos]
+        pos += 1
+        items.pop(0)
 
+    return dict
 
 def invert_dictionary(dictionary):
     """
@@ -57,3 +94,9 @@ def invert_dictionary(dictionary):
         invert_dictionary({1: 2, 3: 4}) == {2: 1, 4: 3}
         invert_dictionary({1: 2, 3: 2}) is None
     """
+    inverted_dictionary = {}
+    for key, value in dictionary.items():
+        if value in inverted_dictionary:
+            return None
+        inverted_dictionary[value] = key
+    return inverted_dictionary
